c87680c21218b6dd57f000d8fd3b561f0aad8fbd
syarii <110132870+syarii@users.noreply.github.com>
2022-09-21 00:53:05 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -9,15 +9,15 @@ let handler = async (m, { conn }) => {
 ┌「 *Sewa bot* 」
 │     
 ├ 1 Grup / 30 Hari
+├ Rp. 15,000 Dana
+├ Rp. 20,000 Pulsa Indosat
+│
+├ 1 Premium / 30 Hari
 ├ Rp. 10,000 Gopay
 ├ Rp. 15,000 Pulsa Three
 │
-├ 1 Premium / 30 Hari
-├ Rp. 5,000 Gopay
-├ Rp. 10,000 Pulsa Three
-│
 ├ tertarik? hubungi: 
-├ @62895336282144 (The.sad.boy01)
+├ @6285759681568 (The.sad.boy01)
 └────
 `.trim(),
         "listType": "PRODUCT_LIST",
@@ -36,9 +36,9 @@ let handler = async (m, { conn }) => {
                 "productId": "4730029423700586",
                 "jpegThumbnail": baper
             },
-            "businessOwnerJid": "6288217277973@s.whatsapp.net"
+            "businessOwnerJid": "6285759681568@s.whatsapp.net"
         },
-        "footerText": "https://wa.me/c/6288217277973\n\nowner number : wa.me/62895336282144"
+        "footerText": "https://wa.me/c/6285759681568\n\nowner number : wa.me/62895336282144"
     }
     conn.sendMessage(m.chat, listMessage, 'listMessage', { quoted: m, contextInfo: { mentionedJid: conn.parseMention(listMessage.description) } })
 }
